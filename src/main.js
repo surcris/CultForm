639ec9ca2d270ec39b438ea20cbeb6a4a1401201
surcris <sympta113@gmail.com>
2023-03-13 16:46:18 +0100

    modif

--- src/main.js
+++ src/main.js
@@ -1,9 +1,9 @@
 import { createApp } from 'vue'
-import registerServiceWorker from './services/registerServiceWorker'
+//import registerServiceWorker from './services/registerServiceWorker'
 import router from './router'
 import App from './App.vue'
 
 
-registerServiceWorker()
+//registerServiceWorker()
 
 createApp(App).use(router).mount('#app')
